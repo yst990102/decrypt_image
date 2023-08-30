b2264791d714a8ae1dd8a92bcfb790e3390ae34a
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2023-08-31 01:41:43 +1000

    init update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,62 @@
-from flask import Flask
+from flask import Flask, Response, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def encrypt_url(key, image_url):
+    # Fetch the image from the URL
+    response = requests.get(image_url)
+    image = response.content
+    
+    # converting image into byte array to
+    # perform encryption easily on numeric data
+    image = bytearray(image)
 
-@app.route('/about')
-def about():
-    return 'About'
+    # performing XOR operation on each value of bytearray
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+
+    print('Encryption Done...')
+    return bytes(image)
+
+def decrypt_url(key, image_url):
+    # Fetch the image from the URL
+    response = requests.get(image_url)
+    image = response.content
+
+    # converting image into byte array to perform decryption easily on numeric data
+    image = bytearray(image)
+
+    # performing XOR operation on each value of bytearray
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+
+    print('Decryption Done...')
+    return bytes(image)
+
+@app.route('/decrypt_image', methods=['GET'])
+def decrypt_image_route():
+    try:
+        key = int(request.args.get('key'))
+        image_url = request.args.get('image_url')
+
+        decrypted_image = decrypt_url(key, image_url)
+
+        return Response(decrypted_image, content_type='image/webp')
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+@app.route('/encrypt_image', methods=['GET'])
+def encrypt_image_route():
+    try:
+        key = int(request.args.get('key'))
+        image_url = request.args.get('image_url')
+
+        encrypted_image = encrypt_url(key, image_url)
+
+        return Response(encrypted_image, content_type='image/webp')
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+if __name__ == '__main__':
+    app.run()
